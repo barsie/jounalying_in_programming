d85a989e8239c57f0853a7cca601fe424fdf9be6
barsie <barsiebarila@gmail.com>
2026-01-15 17:47:09 +0100

    First commit con restructuring my coding journey

---
+++ CodingReact/CalculatorApp/src/App.js
@@ -0,0 +1,66 @@
+import {
+  useState,
+  useRef
+} from "react"; 
+import "./App.css";
+
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
+ 
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value)); 
+  }; 
+ 
+  function minus(e) { 
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+ 
+  function times(e) { 
+    e.preventDefault()
+    setResult((result) => result * Number(inputRef.current.value))
+  }; 
+ 
+  function divide(e) { 
+    e.preventDefault()
+    setResult((result) => result / Number(inputRef.current.value))
+  };
+ 
+  function resetInput(e) { 
+    // Add the code for the resetInput function 
+  }; 
+ 
+  function resetResult(e) { 
+  	// Add the code for the resetResult function 
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}> 
+          {/* add the value of the current total */} 
+        </p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>add</button> 
+        <button onClick={minus}>add</button> 
+        <button onClick={times}>add</button> 
+        <button onClick={divide}>add</button> 
+        <button onClick={resetInput}>add</button> 
+        <button onClick={resetResult}>add</button>
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
